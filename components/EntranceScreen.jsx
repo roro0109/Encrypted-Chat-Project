418a83b57039e5cdf84fcde44e4bbced95a2a657
nadavsan <94635280+nadavsan@users.noreply.github.com>
2022-12-11 22:00:00 +0200

    Add files via upload

---
+++ components/EntranceScreen.jsx
@@ -0,0 +1,27 @@
+import LoginForm from './LoginForm';
+import RegisterFrom from './RegisterForm';
+import { useState } from 'react';
+
+const projectID = '5a16c1de-ea4c-4c12-9b6c-172f530bf6a6';
+
+const EntranceScreen = props => {
+    const [isRegistering, setIsRegistering] = useState(false);
+    const saveUserDetailHandler = enteredUserDetails => {
+        const userDetails = enteredUserDetails
+        setIsRegistering(false);
+    };
+
+    const startRegisteringHandler = () => {
+        setIsRegistering(true);
+    };
+    const stopRegisteringHandler = () => {
+        setIsRegistering(false);
+    };
+    return <div>
+        {!isRegistering && <LoginForm projectID={projectID} startRegistering={startRegisteringHandler}/>}
+        {isRegistering && <RegisterFrom projectID={projectID} userDetails={saveUserDetailHandler} stopRegistering={stopRegisteringHandler}/>}
+    </div>
+
+}
+
+export default EntranceScreen;
