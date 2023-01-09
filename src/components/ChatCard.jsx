85a9f102fbee37204959c6d264fd90f00e8177a6
nadavsan <nadavsan@gmail.com>
2023-01-09 18:09:15 +0200

    chat-list in the making

--- src/components/ChatCard.jsx
+++ src/components/ChatCard.jsx
@@ -4,20 +4,29 @@ import MessageForm from './MessageForm';
 import { decrypt } from './EncryptorDecryptor';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { ChatEngineContext } from 'react-chat-engine';
+//const { htmlToText } = require('html-to-text')
 
 
 const ChatCard = (props) => {
     const chat = props.chat;
     const index = props.index;
+    const { conn, activeChat, setActiveChat } = useContext(ChatEngineContext)
 
+    /*let lastMessage = htmlToText(chat.last_message.text, {})
+    if (!lastMessage) {
+        lastMessage = chat.last_message.attachments.length > 0 ?
+        `${chat.last_message.attachments.length} image${chat.last_message.attachments.length > 1 ? 's' : ''}` :
+        'Say hello!'
+    }*/
 
     if (!chat) return <div />;
 
     return (
-        <div className="chat-card-container">
+        <div className="chat-card-container" onClick={() => setActiveChat(chat.id)}>
             <div className="title-text">{chat?.title}</div>
             <div className="chat-subtitle">
-                {chat.people.map((person) => ` ${person.person.username}`)}
             </div>
         </div>
     );
